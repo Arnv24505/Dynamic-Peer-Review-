16181042716df1bc8e6b4d830d1f75855f68ea9a
Arnv24505 <github24505@gmail.com>
2026-05-09 22:49:05 +0530

    Fixed a enum error regarding the roles

--- server/models/User.js
+++ server/models/User.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['student', 'instructor', 'admin'],
+    enum: ['Student', 'Teacher','Professional', 'admin'],
     default: 'student'
   },
   avatar: {
